ba645e76c9bf72cfdf81e715bdd2116e257ef6d0
Joachim Strömbergson <joachim@assured.se>
2023-01-20 13:32:40 +0100

    Add complete, but untested watchdog functionality
    Signed-off-by: Joachim Strömbergson <joachim@assured.se>

--- hw/application_fpga/core/watchdog/rtl/watchdog.v
+++ hw/application_fpga/core/watchdog/rtl/watchdog.v
@@ -23,33 +23,41 @@ module watchdog(
 		input wire  [7 : 0]  address,
 		input wire  [31 : 0] write_data,
 		output wire [31 : 0] read_data,
-		output wire          ready
+		output wire          ready,
+
+		output wire          timeout
 	       );
 
 
   //----------------------------------------------------------------
   // Internal constant and parameter definitions.
   //----------------------------------------------------------------
-  localparam ADDR_CTRL        = 8'h08;
+  // API
+  localparam ADDR_CTRL          = 8'h08;
+  localparam CTRL_START_BIT     = 0;
+  localparam CTRL_STOP_BIT      = 1;
 
-  localparam ADDR_STATUS      = 8'h09;
-  localparam STATUS_READY_BIT = 0;
+  localparam ADDR_STATUS        = 8'h09;
+  localparam STATUS_RUNNING_BIT = 0;
 
-  localparam ADDR_PRESCALER   = 8'h0a;
-  localparam ADDR_WATCHDOG    = 8'h0b;
+  localparam ADDR_TIMER_INIT    = 8'h0a;
+  localparam ADDR_CURR_TIMER    = 8'h0b;
+
+  // At 18 MHz the default timeout value corresponds to 7.45s.
+  localparam DEFAULT_TIMEOUT_VALUE = 28'h7ff_ffff;
 
 
   //----------------------------------------------------------------
   // Registers including update variables and write enable.
   //----------------------------------------------------------------
-  reg [31 : 0] prescaler_reg;
-  reg          prescaler_we;
+  reg [27 : 0] timer_init_reg;
+  reg          timer_init_we;
 
-  reg [31 : 0] watchdog_reg;
-  reg          watchdog_we;
+  reg          start_reg;
+  reg          start_new;
 
-  reg          start_stop_reg;
-  reg          start_stop_new;
+  reg          stop_reg;
+  reg          stop_new;
 
 
   //----------------------------------------------------------------
@@ -58,15 +66,17 @@ module watchdog(
   reg [31 : 0]  tmp_read_data;
   reg           tmp_ready;
 
-  wire          core_ready;
-  wire [31 : 0] core_curr_watchdog;
+  wire [27 : 0] core_curr_timer;
+  wire          core_running;
+  wire          core_timeout;
 
 
   //----------------------------------------------------------------
   // Concurrent connectivity for ports etc.
   //----------------------------------------------------------------
   assign read_data = tmp_read_data;
-  assign ready = tmp_ready;
+  assign ready     = tmp_ready;
+  assign timeout   = core_timeout;
 
 
   //----------------------------------------------------------------
@@ -76,90 +86,80 @@ module watchdog(
                      .clk(clk),
                      .reset_n(reset_n),
 
-                     .prescaler_init(prescaler_reg),
-                     .watchdog_init(watchdog_reg),
-                     .start_stop(start_stop_reg),
+                     .timer_init(timer_init_reg),
+                     .start(start_reg),
+                     .stop(stop_reg),
 
-		     .curr_watchdog(core_curr_watchdog),
-                     .ready(core_ready)
+		     .curr_timer(core_curr_timer),
+                     .running(core_running),
+                     .timeout(core_timeout)
                     );
 
 
   //----------------------------------------------------------------
-  // reg_update
+  // reg_update.
   //----------------------------------------------------------------
   always @ (posedge clk)
     begin : reg_update
       if (!reset_n) begin
-	start_stop_reg <= 1'h0;
-	prescaler_reg  <= 32'h0;
-	watchdog_reg   <= 32'h0;
+	start_reg      <= 1'h0;
+	stop_reg       <= 1'h0;
+	timer_init_reg <= DEFAULT_TIMEOUT_VALUE;
       end
+
       else begin
-	start_stop_reg <= start_stop_new;
+	start_reg <= start_new;
+	stop_reg  <= stop_new;
 
-	if (prescaler_we) begin
-	  prescaler_reg <= write_data;
-	end
-
-	if (watchdog_we) begin
-	  watchdog_reg <= write_data;
+	if (timer_init_we) begin
+	  timer_init_reg <= write_data[27 : 0];
 	end
       end
     end // reg_update
 
 
   //----------------------------------------------------------------
-  // api
-  //
-  // The interface command decoding logic.
+  // api.
   //----------------------------------------------------------------
   always @*
     begin : api
-      start_stop_new = 1'h0;
-      prescaler_we   = 1'h0;
-      watchdog_we    = 1'h0;
-      tmp_read_data  = 32'h0;
-      tmp_ready      = 1'h0;
+      start_new     = 1'h0;
+      stop_new      = 1'h0;
+      timer_init_we = 1'h0;
+      tmp_read_data = 32'h0;
+      tmp_ready     = 1'h0;
 
       if (cs) begin
 	tmp_ready = 1'h1;
 
         if (we) begin
           if (address == ADDR_CTRL) begin
-	    start_stop_new = 1'h1;
+	    start_new = write_data[CTRL_START_BIT];
+	    stop_new  = write_data[CTRL_STOP_BIT];
 	  end
 
-	  if (core_ready) begin
-            if (address == ADDR_PRESCALER) begin
-	      prescaler_we = 1'h1;
-	    end
-
-            if (address == ADDR_WATCHDOG) begin
-	      watchdog_we = 1'h1;
+          if (address == ADDR_TIMER_INIT) begin
+	    if (!core_running) begin
+	      timer_init_we = 1'h1;
 	    end
 	  end
-        end
+	end
 
-        else begin
+	else begin
 	  if (address == ADDR_STATUS) begin
-	    tmp_read_data = {31'h0, core_ready};
+	    tmp_read_data[STATUS_RUNNING_BIT] = core_running;
 	  end
 
-	  if (address == ADDR_PRESCALER) begin
-	    tmp_read_data = prescaler_reg;
+          if (address == ADDR_TIMER_INIT) begin
+	    tmp_read_data[27 : 0] = timer_init_reg;
 	  end
 
-	  if (address == ADDR_WATCHDOG) begin
-	    if (core_ready) begin
-	      tmp_read_data = watchdog_reg;
-	    end else begin
-	      tmp_read_data = core_curr_watchdog;
-	    end
+	  if (address == ADDR_CURR_TIMER) begin
+	    tmp_read_data[27 : 0] = core_curr_timer;
 	  end
         end
       end
-    end // addr_decoder
+    end // api
 endmodule // watchdog
 
 //======================================================================
--- hw/application_fpga/core/watchdog/rtl/watchdog_core.v
+++ hw/application_fpga/core/watchdog/rtl/watchdog_core.v
@@ -14,15 +14,17 @@
 `default_nettype none
 
 module watchdog_core(
-                  input wire           clk,
-                  input wire           reset_n,
+                     input wire           clk,
+                     input wire           reset_n,
 
-                  input wire [31 : 0]  prescaler_init,
-                  input wire [31 : 0]  watchdog_init,
-                  input wire           start_stop,
+                     input wire [27 : 0]  timer_init,
+                     input wire           start,
+                     input wire           stop,
 
-                  output wire [31 : 0] curr_watchdog,
-                  output wire          ready
+                     output wire [27 : 0] curr_timer,
+                     output wire          running,
+
+		     output wire          timeout
                   );
 
 
@@ -30,28 +32,26 @@ module watchdog_core(
   // Internal constant and parameter definitions.
   //----------------------------------------------------------------
   localparam CTRL_IDLE      = 2'h0;
-  localparam CTRL_PRESCALER = 2'h1;
-  localparam CTRL_WATCHDOG  = 2'h2;
+  localparam CTRL_START     = 2'h1;
+  localparam CTRL_COUNTDOWN = 2'h2;
+  localparam CTRL_TIMEOUT   = 2'h3;
 
 
   //----------------------------------------------------------------
   // Registers including update variables and write enable.
   //----------------------------------------------------------------
-  reg          ready_reg;
-  reg          ready_new;
-  reg          ready_we;
+  reg          running_reg;
+  reg          running_new;
+  reg          running_we;
 
-  reg [31 : 0] prescaler_reg;
-  reg [31 : 0] prescaler_new;
-  reg          prescaler_we;
-  reg          prescaler_set;
-  reg          prescaler_dec;
+  reg [27 : 0] timer_reg;
+  reg [27 : 0] timer_new;
+  reg          timer_set;
+  reg          timer_dec;
+  reg          timer_we;
 
-  reg [31 : 0] watchdog_reg;
-  reg [31 : 0] watchdog_new;
-  reg          watchdog_we;
-  reg          watchdog_set;
-  reg          watchdog_dec;
+  reg          timeout_reg;
+  reg          timeout_new;
 
   reg [1 : 0]  core_ctrl_reg;
   reg [1 : 0]  core_ctrl_new;
@@ -61,8 +61,9 @@ module watchdog_core(
   //----------------------------------------------------------------
   // Concurrent connectivity for ports etc.
   //----------------------------------------------------------------
-  assign curr_watchdog = watchdog_reg;
-  assign ready      = ready_reg;
+  assign curr_timer = timer_reg;
+  assign running    = running_reg;
+  assign timeout    = timeout_reg;
 
 
   //----------------------------------------------------------------
@@ -72,23 +73,21 @@ module watchdog_core(
     begin: reg_update
       if (!reset_n)
         begin
-          ready_reg     <= 1'h1;
-	  prescaler_reg <= 32'h0;
-	  watchdog_reg     <= 32'h0;
+          running_reg   <= 1'h0;
+	  timer_reg     <= 28'h0;
+	  timeout_reg   <= 1'h0;
           core_ctrl_reg <= CTRL_IDLE;
         end
       else
         begin
-          if (ready_we) begin
-            ready_reg <= ready_new;
+	  timeout_reg <= timeout_new;
+
+          if (running_we) begin
+            running_reg <= running_new;
 	  end
 
-	  if (prescaler_we) begin
-	    prescaler_reg <= prescaler_new;
-	  end
-
-	  if (watchdog_we) begin
-	    watchdog_reg <= watchdog_new;
+	  if (timer_we) begin
+	    timer_reg <= timer_new;
 	  end
 
           if (core_ctrl_we) begin
@@ -99,39 +98,21 @@ module watchdog_core(
 
 
   //----------------------------------------------------------------
-  // prescaler_ctr
+  // timer_ctr
   //----------------------------------------------------------------
   always @*
-    begin : prescaler_ctr
-      prescaler_new = 32'h0;
-      prescaler_we  = 1'h0;
+    begin : timer_ctr
+      timer_new = 28'h0;
+      timer_we  = 1'h0;
 
-      if (prescaler_set) begin
-	prescaler_new = prescaler_init;
-	prescaler_we  = 1'h1;
+      if (timer_set) begin
+	timer_new = timer_init;
+	timer_we  = 1'h1;
       end
-      else if (prescaler_dec) begin
-	prescaler_new = prescaler_reg - 1'h1;
-	prescaler_we  = 1'h1;
-      end
-    end
 
-
-  //----------------------------------------------------------------
-  // watchdog_ctr
-  //----------------------------------------------------------------
-  always @*
-    begin : watchdog_ctr
-      watchdog_new = 32'h0;
-      watchdog_we  = 1'h0;
-
-      if (watchdog_set) begin
-	watchdog_new = watchdog_init;
-	watchdog_we  = 1'h1;
-      end
-      else if (watchdog_dec) begin
-	watchdog_new = watchdog_reg - 1'h1;
-	watchdog_we  = 1'h1;
+      else if (timer_dec) begin
+	timer_new = timer_reg - 1'h1;
+	timer_we  = 1'h1;
       end
     end
 
@@ -141,80 +122,53 @@ module watchdog_core(
   //----------------------------------------------------------------
   always @*
     begin : core_ctrl
-      ready_new     = 1'h0;
-      ready_we      = 1'h0;
-      prescaler_set = 1'h0;
-      prescaler_dec = 1'h0;
-      watchdog_set     = 1'h0;
-      watchdog_dec     = 1'h0;
+      timer_set     = 1'h0;
+      timer_dec     = 1'h0;
+      running_new   = 1'h0;
+      running_we    = 1'h0;
+      timeout_new   = 1'h0;
       core_ctrl_new = CTRL_IDLE;
       core_ctrl_we  = 1'h0;
 
       case (core_ctrl_reg)
         CTRL_IDLE: begin
-          if (start_stop)
-            begin
-              ready_new     = 1'h0;
-              ready_we      = 1'h1;
-	      prescaler_set = 1'h1;
-	      watchdog_set     = 1'h1;
-	      if (prescaler_init == 0) begin
-		core_ctrl_new = CTRL_WATCHDOG;
-		core_ctrl_we  = 1'h1;
-		end else begin
-		  core_ctrl_new = CTRL_PRESCALER;
-		  core_ctrl_we  = 1'h1;
-		end
-            end
+          if (start) begin
+            running_new   = 1'h1;
+            running_we    = 1'h1;
+	    core_ctrl_new = CTRL_START;
+	    core_ctrl_we  = 1'h1;
+	  end
         end
 
+        CTRL_START: begin
+	  timer_set     = 1'h1;
+	  core_ctrl_new = CTRL_COUNTDOWN;
+	  core_ctrl_we  = 1'h1;
+        end
 
-	CTRL_PRESCALER: begin
-	  if (start_stop) begin
-            ready_new     = 1'h1;
-            ready_we      = 1'h1;
-            core_ctrl_new = CTRL_IDLE;
+	CTRL_COUNTDOWN: begin
+	  timer_dec = 1'h1;
+	  if (stop) begin
+            running_new   = 1'h0;
+            running_we    = 1'h1;
+	    core_ctrl_new = CTRL_IDLE;
+	  end
+
+	  else if (start) begin
+	    core_ctrl_we  = 1'h1;
+            core_ctrl_new = CTRL_START;
             core_ctrl_we  = 1'h1;
 	  end
 
-	  else begin
-	    if (prescaler_reg == 1) begin
-              core_ctrl_new = CTRL_WATCHDOG;
-              core_ctrl_we  = 1'h1;
-	    end else begin
-	      prescaler_dec = 1'h1;
-	    end
-	  end
-	end
-
-
-	CTRL_WATCHDOG: begin
-	  if (start_stop) begin
-            ready_new     = 1'h1;
-            ready_we      = 1'h1;
-            core_ctrl_new = CTRL_IDLE;
+	  else if (timer_reg == 28'h0) begin
+            core_ctrl_new = CTRL_TIMEOUT;
             core_ctrl_we  = 1'h1;
 	  end
-
-	  else begin
-	    if (watchdog_reg == 1) begin
-              ready_new     = 1'h1;
-              ready_we      = 1'h1;
-              core_ctrl_new = CTRL_IDLE;
-              core_ctrl_we  = 1'h1;
-	    end
-
-	    else begin
-	      watchdog_dec = 1'h1;
-
-	      if (prescaler_init > 0) begin
-		prescaler_set = 1'h1;
-		core_ctrl_new = CTRL_PRESCALER;
-		core_ctrl_we  = 1'h1;
-	      end
-	    end
-	  end
 	end
+
+        CTRL_TIMEOUT: begin
+	  timeout_new = 1'h1;
+        end
 
         default: begin
         end
